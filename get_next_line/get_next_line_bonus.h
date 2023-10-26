8c639a589553d7c65ef1cf43da528356eb436e7d
Loris <loris.kary67730@gmqil.com>
2023-10-26 10:55:46 +0200

    s

--- get_next_line/get_next_line_bonus.h
+++ get_next_line/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 09:30:44 by loris             #+#    #+#             */
-/*   Updated: 2023/10/26 10:06:09 by loris            ###   ########.fr       */
+/*   Updated: 2023/10/26 10:15:02 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
--- get_next_line/get_next_line_utils_bonus.c
+++ get_next_line/get_next_line_utils_bonus.c
@@ -1,16 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: loris <loris@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 15:20:38 by loris             #+#    #+#             */
-/*   Updated: 2023/10/26 10:13:10 by loris            ###   ########.fr       */
+/*   Updated: 2023/10/26 10:14:03 by loris            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	creat_next_line(t_list **lst, int fd)
+{
+	char			*buff;
+	int				n;
+
+	while (ft_lst_check(lst))
+	{
+		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buff)
+			return ;
+		n = read(fd, buff, BUFFER_SIZE);
+		if (n <= 0)
+		{
+			free(buff);
+			return ;
+		}
+		buff[n] = '\0';
+		ft_lstadd_back(lst, ft_lstnew(buff));
+	}
+}
 
 t_list	*ft_lstnew(char *content)
 {
@@ -73,25 +94,4 @@ int	ft_lst_check(t_list **lst)
 		temp = temp->next;
 	}
 	return (1);
-}
-
-void	creat_next_line(t_list **lst, int fd)
-{
-	char			*buff;
-	int				n;
-
-	while (ft_lst_check(lst))
-	{
-		buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		if (!buff)
-			return ;
-		n = read(fd, buff, BUFFER_SIZE);
-		if (n <= 0)
-		{
-			free(buff);
-			return ;
-		}
-		buff[n] = '\0';
-		ft_lstadd_back(lst, ft_lstnew(buff));
-	}
 }
