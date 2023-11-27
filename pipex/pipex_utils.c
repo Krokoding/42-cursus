07e4b9ac7f30e4c8da0ba8eed3efdfb656247c41
Loris Kary-Potes <lkary-po@k1r2p5.42mulhouse.fr>
2023-11-27 15:03:02 +0100

    e

--- pipex/pipex_utils.c
+++ pipex/pipex_utils.c
@@ -1,31 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkary-po <lkary-po@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/24 11:20:54 by loris             #+#    #+#             */
-/*   Updated: 2023/11/27 11:19:30 by lkary-po         ###   ########.fr       */
+/*   Created: 2023/11/27 13:04:31 by lkary-po          #+#    #+#             */
+/*   Updated: 2023/11/27 13:04:32 by lkary-po         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-
-#include <stdio.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include "libft.h"
-
-
-int		put_in_pipe(int **fd, char **av);
-char	*path_creator(char *program_name);
-int		put_in_file(int	**fd, char **av);
-int		cat_first_file(int **fd, char **av);
-
-#endif
